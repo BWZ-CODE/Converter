c88f83ac1942baa6f7d83383ab559b81f2ae36d4
BWZ-CODE <mmige24@gmail.com>
2025-09-15 18:55:30 -0500

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Miguel Angel Bohorquez on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
